44681e83ad23f5d3e51aedd8d9da648d4424f98e
Musooo <nicoloblack2@gmail.com>
2024-12-27 05:37:29 -0500

    available check, created macros for row,column and empty space for the field

--- src/game.c
+++ src/game.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define EMPTY ' '
+#define ROW 5
+#define COLUMN 5
 /*
 0|0|0
 -----
@@ -18,11 +21,11 @@ typedef struct {
 void init_field(field* f){
     for (int i = 0; i < 5; i++){
         if (i%2==0){
-            f->field[i][0] = '0';
+            f->field[i][0] = EMPTY;
             f->field[i][1] = '|';
-            f->field[i][2] = '0';
+            f->field[i][2] = EMPTY;
             f->field[i][3] = '|';
-            f->field[i][4] = '0';
+            f->field[i][4] = EMPTY;
             f->field[i][5] = '\0';
         }else{
             f->field[i][0] = '-';
@@ -38,18 +41,42 @@ void init_field(field* f){
 void print_field(field* f){
     printf("\033[32m\033[H\033[2J");
     for (int i = 0; i < 5; i++){
-        printf("%s\n", f->field[i]);
+        printf("\033[3%dm\%s\n", i+1,f->field[i]);
     }
+}
+
+void insert_choice(field* f, char *choice){
+    switch (choice[0])
+    {
+    case 'A':
+        break;
+    case 'B':
+        break;
+    default:
+        printf("who is this???");
+        break;
+    }
+}
+
+int is_available(field* f, char *choice){
+    if (f->field[atoi(&choice[0])][atoi(&choice[1])] != EMPTY){
+        printf("not empty");
+        return 1;
+    }
+    return 0;
 }
 
 int main(){
     field *ptrf = (field *)malloc(sizeof(field));
     init_field(ptrf);
-    for (int i = 0; i< 4; i++){
-        printfield(ptrf);
-        printf("\n%d", i);
-        fflush(stdout);
-        sleep(3);
-    }
+    // for (int i = 0; i< 4; i++){
+    //     print_field(ptrf);
+    //     printf("\n%d", i);
+    //     fflush(stdout);
+    //     sleep(3);
+    // }
+
+    printf("%d\n", is_available(ptrf, "00"));
+
     return 0;
 }
