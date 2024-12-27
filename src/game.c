b3f0dccef9ade07f53c9c6b5588e2f941bdbf71c
Musooo <nicoloblack2@gmail.com>
2024-12-27 09:08:17 -0500

    added my own string to int conversion because atoi doesn't function properly

--- src/game.c
+++ src/game.c
@@ -7,6 +7,7 @@
 #define COLUMN 5
 #define SELECT_BOX(f, row, column) (f->field[row][column])
 #define SELECT_BOX_WITH_CHAR(f, choice) (f->field[atoi(&choice[1])][atoi(&choice[2])])
+
 /*
 0|0|0
 -----
@@ -21,39 +22,40 @@ typedef struct {
 
 
 void init_field(field* f){
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < ROW; i++){
         if (i%2==0){
-            f->field[i][0] = EMPTY;
-            f->field[i][1] = '|';
-            f->field[i][2] = EMPTY;
-            f->field[i][3] = '|';
-            f->field[i][4] = EMPTY;
-            f->field[i][5] = '\0';
+            SELECT_BOX(f,i,0) = EMPTY;
+            SELECT_BOX(f,i,1) = '|';
+            SELECT_BOX(f,i,2) = EMPTY;
+            SELECT_BOX(f,i,3) = '|';
+            SELECT_BOX(f,i,4) = EMPTY;
+            SELECT_BOX(f,i,5) = '\0';
         }else{
-            f->field[i][0] = '-';
-            f->field[i][1] = '-';
-            f->field[i][2] = '-';
-            f->field[i][3] = '-';
-            f->field[i][4] = '-';
-            f->field[i][5] = '\0';
+            SELECT_BOX(f,i,0) = '-';
+            SELECT_BOX(f,i,1) = '-';
+            SELECT_BOX(f,i,2) = '-';
+            SELECT_BOX(f,i,3) = '-';
+            SELECT_BOX(f,i,4) = '-';
+            SELECT_BOX(f,i,5) = '\0';
         }
     }//i don't know man, i'm to stubborn tu use 2 for loops
 }
 
 void print_field(field* f){
     printf("\033[32m\033[H\033[2J");
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < ROW; i++){
         printf("\033[3%dm\%s\n", i+1,f->field[i]);
     }
 }
 
-void insert_choice(field* f, char *choice){
+void insert_choice(field *f, char *choice){
     switch (choice[0])
     {
     case 'A':
-        SELECT_BOX_WITH_CHAR(f, choice);
+        SELECT_BOX_WITH_CHAR(f, choice) = 'X';
         break;
     case 'B':
+        SELECT_BOX_WITH_CHAR(f, choice) = 'O';
         break;
     default:
         printf("who is this???");
@@ -63,18 +65,36 @@ void insert_choice(field* f, char *choice){
 
 int is_available(field* f, char *choice){
     if (SELECT_BOX_WITH_CHAR(f, choice) != EMPTY){
-        printf("not empty");
+        printf("not empty\n");
         return 1;
     }
     return 0;
 }
 
-//choice 'PLAYERID(A or B)ROWCOLUMN[1,3,5][1,3,5])'
+int pow(int n,int b){
+    if (b==0){
+        return 1;
+    }
+    return n*pow(n,b-1);
+}
+
+int stringtoint(char *arr,int pos,int index){
+    if (pos==0){
+        return 0;
+    }
+
+    return (arr[index]-48)*pow(10,pos-1)+stringtoint(arr,pos-1,index+1);
+}
+
+//choice 'PLAYERID(A or B)ROWCOLUMN[0,2,4][0,2,4])'
 int main(){
+    char *c = "A02";
     field *ptrf = (field *)malloc(sizeof(field));
     init_field(ptrf);
-
-    printf("%d\n", is_available(ptrf, "00"));
-
+    SELECT_BOX(ptrf,atoi(&c[1]),atoi(&c[2])) = 'X';
+    //SELECT_BOX_WITH_CHAR(ptrf, c) = 'O';
+    print_field(ptrf);
+    printf("%c\n%c\n", c[1], c[2]);
+    printf("%d\n%d\n", stringtoint((char*) &c[1],1,0),stringtoint((char*) &c[2],1,0));
     return 0;
 }
